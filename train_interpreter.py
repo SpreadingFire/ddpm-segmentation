438c2ccfcf3fe1c2a2987f2b749e203dc348c659
SpreadingFire <118586312+SpreadingFire@users.noreply.github.com>
2024-06-17 18:57:56 -0400

    Update train_interpreter.py

--- train_interpreter.py
+++ train_interpreter.py
@@ -9,30 +9,32 @@ from torch.utils.data import DataLoader
 
 import argparse
 from src.utils import setup_seed, multi_acc
-from src.pixel_classifier import  load_ensemble, compute_iou, predict_labels, save_predictions, save_predictions, pixel_classifier
+from src.pixel_classifier import load_ensemble, compute_iou, predict_labels, save_predictions, pixel_classifier
 from src.datasets import ImageLabelDataset, FeatureDataset, make_transform
 from src.feature_extractors import create_feature_extractor, collect_features
 
 from guided_diffusion.guided_diffusion.script_util import model_and_diffusion_defaults, add_dict_to_argparser
 from guided_diffusion.guided_diffusion.dist_util import dev
 
-
+# 准备数据集
 def prepare_data(args):
+    # 创建特征提取器
     feature_extractor = create_feature_extractor(**args)
     
-    print(f"Preparing the train set for {args['category']}...")
+    print(f"准备训练集: {args['category']}...")
+    # 创建图像标签数据集
     dataset = ImageLabelDataset(
         data_dir=args['training_path'],
         resolution=args['image_size'],
         num_images=args['training_number'],
-        transform=make_transform(
-            args['model_type'],
-            args['image_size']
-        )
+        transform=make_transform(args['model_type'], args['image_size'])
     )
+    
+    # 初始化特征和标签张量
     X = torch.zeros((len(dataset), *args['dim'][::-1]), dtype=torch.float)
     y = torch.zeros((len(dataset), *args['dim'][:-1]), dtype=torch.uint8)
 
+    # 如果指定了共享噪声
     if 'share_noise' in args and args['share_noise']:
         rnd_gen = torch.Generator(device=dev()).manual_seed(args['seed'])
         noise = torch.randn(1, 3, args['image_size'], args['image_size'], 
@@ -45,30 +47,30 @@ def prepare_data(args):
         features = feature_extractor(img, noise=noise)
         X[row] = collect_features(args, features).cpu()
         
+        # 删除标注数量少于20的标签
         for target in range(args['number_class']):
             if target == args['ignore_label']: continue
             if 0 < (label == target).sum() < 20:
-                print(f'Delete small annotation from image {dataset.image_paths[row]} | label {target}')
+                print(f'删除小于20的标注: 图片 {dataset.image_paths[row]} 标签 {target}')
                 label[label == target] = args['ignore_label']
         y[row] = label
     
     d = X.shape[1]
-    print(f'Total dimension {d}')
+    print(f'总维度 {d}')
     X = X.permute(1,0,2,3).reshape(d, -1).permute(1, 0)
     y = y.flatten()
+
     return X[y != args['ignore_label']], y[y != args['ignore_label']]
 
 
+# 评估模型
 def evaluation(args, models):
     feature_extractor = create_feature_extractor(**args)
     dataset = ImageLabelDataset(
         data_dir=args['testing_path'],
         resolution=args['image_size'],
         num_images=args['testing_number'],
-        transform=make_transform(
-            args['model_type'],
-            args['image_size']
-        )
+        transform=make_transform(args['model_type'], args['image_size'])
     )
 
     if 'share_noise' in args and args['share_noise']:
@@ -85,30 +87,28 @@ def evaluation(args, models):
         features = collect_features(args, features)
 
         x = features.view(args['dim'][-1], -1).permute(1, 0)
-        pred, uncertainty_score = predict_labels(
-            models, x, size=args['dim'][:-1]
-        )
+        pred, uncertainty_score = predict_labels(models, x, size=args['dim'][:-1])
         gts.append(label.numpy())
         preds.append(pred.numpy())
         uncertainty_scores.append(uncertainty_score.item())
     
     save_predictions(args, dataset.image_paths, preds)
     miou = compute_iou(args, preds, gts)
-    print(f'Overall mIoU: ', miou)
-    print(f'Mean uncertainty: {sum(uncertainty_scores) / len(uncertainty_scores)}')
+    print(f'总mIoU: ', miou)
+    print(f'不确定性均值: {sum(uncertainty_scores) / len(uncertainty_scores)}')
 
 
-# Adopted from https://github.com/nv-tlabs/datasetGAN_release/blob/d9564d4d2f338eaad78132192b865b6cc1e26cac/datasetGAN/train_interpreter.py#L434
+# 训练模型
 def train(args):
     features, labels = prepare_data(args)
     train_data = FeatureDataset(features, labels)
 
-    print(f" ********* max_label {args['number_class']} *** ignore_label {args['ignore_label']} ***********")
-    print(f" *********************** Current number data {len(features)} ***********************")
+    print(f" ********* 最大标签 {args['number_class']} *** 忽略标签 {args['ignore_label']} ***********")
+    print(f" *********************** 当前数据数量 {len(features)} ***********************")
 
     train_loader = DataLoader(dataset=train_data, batch_size=args['batch_size'], shuffle=True, drop_last=True)
 
-    print(" *********************** Current dataloader length " +  str(len(train_loader)) + " ***********************")
+    print(" *********************** 当前数据加载器长度 " +  str(len(train_loader)) + " ***********************")
     for MODEL_NUMBER in range(args['start_model_num'], args['model_num'], 1):
 
         gc.collect()
@@ -150,7 +150,7 @@ def train(args):
 
                     if break_count > 50:
                         stop_sign = 1
-                        print("*************** Break, Total iters,", iteration, ", at epoch", str(epoch), "***************")
+                        print("*************** 终止, 总迭代次数,", iteration, ", 在 epoch", str(epoch), "***************")
                         break
 
             if stop_sign == 1:
@@ -159,10 +159,9 @@ def train(args):
         model_path = os.path.join(args['exp_dir'], 
                                   'model_' + str(MODEL_NUMBER) + '.pth')
         MODEL_NUMBER += 1
-        print('save to:',model_path)
-        torch.save({'model_state_dict': classifier.state_dict()},
-                   model_path)
-    
+        print('保存到:',model_path)
+        torch.save({'model_state_dict': classifier.state_dict()}, model_path)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -174,12 +173,12 @@ if __name__ == '__main__':
     args = parser.parse_args()
     setup_seed(args.seed)
 
-    # Load the experiment config
+    # 加载实验配置
     opts = json.load(open(args.exp, 'r'))
     opts.update(vars(args))
     opts['image_size'] = opts['dim'][0]
 
-    # Prepare the experiment folder 
+    # 准备实验文件夹 
     if len(opts['steps']) > 0:
         suffix = '_'.join([str(step) for step in opts['steps']])
         suffix += '_' + '_'.join([str(step) for step in opts['blocks']])
@@ -187,18 +186,18 @@ if __name__ == '__main__':
 
     path = opts['exp_dir']
     os.makedirs(path, exist_ok=True)
-    print('Experiment folder: %s' % (path))
+    print('实验文件夹: %s' % (path))
     os.system('cp %s %s' % (args.exp, opts['exp_dir']))
 
-    # Check whether all models in ensemble are trained 
+    # 检查是否所有的模型都已经被训练 
     pretrained = [os.path.exists(os.path.join(opts['exp_dir'], f'model_{i}.pth')) 
                   for i in range(opts['model_num'])]
               
     if not all(pretrained):
-        # train all remaining models
+        # 训练所有剩余的模型
         opts['start_model_num'] = sum(pretrained)
         train(opts)
     
-    print('Loading pretrained models...')
+    print('加载预训练模型...')
     models = load_ensemble(opts, device='cuda')
     evaluation(opts, models)
